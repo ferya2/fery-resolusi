0c8779dfefe2c8c3d6c0b60c1b9ea5dcfeb0b675
Ade Fery Angriawan <93005656+ferya2@users.noreply.github.com>
2021-12-17 15:32:48 +0700

    Coba Array 1 Dimensi

---
+++ Array1dimensi.java
@@ -0,0 +1,28 @@
+public class CobaArray {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] Bulan; //Deklarasi Array
+        
+        Bulan = new String[12]; //Menentukan jumlah elemen dan Panjang Array
+        int[] JumlahHari ={31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        Bulan[0] = "Januari";
+        Bulan[1] = "Februari";
+        Bulan[2] = "Maret";
+        Bulan[3] = "April";
+        Bulan[4] = "Mei";
+        Bulan[5] = "Juni";
+        Bulan[6] = "Juli";
+        Bulan[7] = "Agustus";
+        Bulan[8] = "September";
+        Bulan[9] = "Oktober";
+        Bulan[10]="November";
+        Bulan[11]="Desember";
+        
+        for (int i = 0; i < Bulan.length; i++){
+            System.out.println("Bulan "+Bulan[i]+" Memiliki " + JumlahHari[i]+ " Hari");
+        }
+    }
+    
+}
